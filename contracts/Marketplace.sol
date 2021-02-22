afbfbba57b00192c432f776cc77a9749d4e4d028
ilumin <iolumin@gmail.com>
2021-02-22 15:13:04 +0700

    add: product in marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -2,6 +2,16 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    mapping(uint256 => Product) public products;
+    uint256 public productCount = 0;
+
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        address owner;
+        bool purchased;
+    }
 
     constructor() public {
         name = "DApp Marketplace";
